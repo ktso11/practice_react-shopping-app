fd8946c50db9bfbff909d6b71ab157bdc68b8220
Katie So <katieso86@gmail.com>
2020-01-30 09:25:12 -0800

    working on notification logic

--- src/services/notification-service.js
+++ src/services/notification-service.js
@@ -1,4 +1,16 @@
-let instance = null
+//156 Creating Notification Service
+
+//registering to observe
+//observer is me or the component that wants to listen
+var observers = {};
+
+//example:
+// var observers = {
+//     "wishListChanged": [{observer: someComponent, callBack: someFunction},
+//     {observer: someOtherComponent}],
+//     "userHasLoggedIn":[{observer:something}]
+// };
+let instance = null;
 
 class NotificationService {
     constructor() {
@@ -6,5 +18,13 @@ class NotificationService {
             instance = this;
         }
         return instance;
+    }
+
+    addObserver = (notifName, observer, callBack) => {
+        let obs = observers[notifName]
+
+        if(!obs) {
+            observers[notifName]= []
+        }
     }
 }
